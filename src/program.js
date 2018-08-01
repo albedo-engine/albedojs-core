c60b8d0053fc2e73d32eca668cdb15b3c4453660
David Peicho <david.peicho@gmail.com>
2018-07-31 21:41:41 -0400

    webglcontext: add program compilation

--- src/program.js
+++ src/program.js
@@ -1,30 +1,9 @@
 export class Program {
 
-  constructor(gl, vertexShader, fragmentShader, uniforms) {
-    this.webglObject = null;
-
-    this.vertexShader = vertexShader;
-    this.fragmentShader = fragmentShader;
+  constructor(gl, vertexSource, fragmentSource, uniforms) {
+    this.vertexShader = vertexSource;
+    this.fragmentShader = fragmentSource;
     this.uniforms = uniforms;
-
-    this._linked = false;
-    this._gl = gl;
-  }
-
-  link() {
-    const program = this._gl.createProgram();
-    this._gl.attachShader(program, this.vertexShader.webglObject);
-    this._gl.attachShader(program, this.fragmentShader.webglObject);
-    this._gl.linkProgram(program);
-
-    const success = this._gl.getProgramParameter(program, this._gl.LINK_STATUS);
-    if (!success) {
-      program = this._gl.getProgramInfoLog(program);
-      this._gl.deleteProgram(program);
-      return;
-    }
-    this._linked = true;
-    this.webglObject = program;
   }
 
 }
--- src/webgl-context.js
+++ src/webgl-context.js
@@ -1,3 +1,6 @@
+import { Cache } from 'utils/cache';
+import { compileShader, getFormattedCode, linkProgram } from 'webgl-shader';
+
 export class WebGLContext {
 
   constructor(options) {
@@ -9,21 +12,48 @@ export class WebGLContext {
     this._verbose = {};
     this._verbose.name = options.name || DEFAULT_NAME;
     this._verbose.logAll = options.verbose || false;
+
+    this._programs = new Cache();
   }
 
-  createVAO() {
+  compile(program) {
+    const vertexShader = compileShader(
+      program.vertexSource,
+      this._gl.VERTEX_SHADER
+    );
+    const fragmentShader = compileShader(
+      program.vertexSource,
+      this._gl.FRAGMENT_SHADER
+    );
+    const vertexFailed = !(vertexShader instanceof WebGLShader);
+    const fragmentFailed = !(fragmentShader instanceof WebGLShader);
+    const failed = vertexFailed && fragmentFailed;
 
+    const programData = this._programs.get(program);
+    programData.failed = failed;
+
+    if (vertexFailed) {
+      const code = getFormattedCode(program.vertexSource);
+      console.warn(`vertex shader ${WARN.SHADER_COMPILE}\n${code}`);
+    }
+    if (fragmentFailed) {
+      const code = getFormattedCode(program.fragmentSource);
+      console.warn(`fragment shader ${WARN.SHADER_COMPILE}\n${code}`);
+    }
+
+    if (failed) return false;
+
+    const webGLObject = linkProgram(vertexShader, fragmentShader);
+    if (!(webGLObject instanceof WebGLProgram)) {
+      console.warn(`${WARN.PROGRAM_LINK}`);
+      return false;
+    }
+
+    programData.webGLObject = webGLObject;
+    return true;
   }
 
-  createVBO() {
-
-  }
-
-  createProgram() {
-
-  }
-
-  draw() {
+  draw(program, vao) {
 
   }
 
@@ -34,6 +64,12 @@ export class WebGLContext {
 }
 
 const DEFAULT_NAME = `Albedo.WebGLContext`;
+
 const ERROR = {
   MISSING_CANVAS: `no canvas provided.`
+};
+
+const WARN = {
+  SHADER_COMPILE: `compilation failed!`,
+  PROGRAM_LINK: `shaders linking failed!`
 };
