6c096068bf52664ac59789b3e19f250d97a0cbbe
David Peicho <david.peicho@gmail.com>
2018-07-31 22:44:37 -0400

    vao: add basic vao setup

--- src/vao.js
+++ src/vao.js
@@ -1,11 +1,26 @@
+class VertexAttrib {
+
+  constructor(vbo, location, offset, normalized) {
+    this.vbo = vbo;
+    this.location = location;
+    this.offset = offset;
+    this.normalized = normalized;
+  }
+
+}
+
 export class VAO {
 
   constructor() {
-
+    this.vertexAttributes = [];
+    this.count = -1;
   }
 
-  update(VBOs) {
-
+  vertexAttrib(vbo, location, offset, normalized) {
+    const attrib = new VertexAttrib(vbo, location, offset, normalized);
+    this.vertexAttributes.push(attrib);
+    if (this.count < 0)
+      this.count = vbo.data.length / vbo.data.components;
   }
 
 }
--- src/webgl-context.js
+++ src/webgl-context.js
@@ -13,6 +13,8 @@ export class WebGLContext {
     this._verbose.name = options.name || DEFAULT_NAME;
     this._verbose.logAll = options.verbose || false;
 
+    this._VBOs = new Cache();
+    this._VAOs = new Cache();
     this._programs = new Cache();
   }
 
@@ -53,8 +55,58 @@ export class WebGLContext {
     return true;
   }
 
-  draw(program, vao) {
+  updateVBO(vbo) {
+    const vboData = this._VBOs.get(vbo);
+    if (vboData.failed) return false;
 
+    if (vboData.webGLObject) return true;
+
+    const drawType = vbo.dynamic ? this._gl.DYNAMC_DRAW : this._gl.STATIC_DRAW;
+
+    const webGLObject = this._gl.createBuffer();
+    this._gl.bindBuffer(webGLObject);
+    this._gl.bufferData(this._gl.ARRAY_BUFFER, vbo.data, drawType);
+
+    return true;
+  }
+
+  updateVAO(vao) {
+    const vaoData = this._VAOs.get(vao);
+    if (vaoData.webGLObject) return true;
+
+    const webGLObject = this._gl.createVertexArray();
+    this._gl.bindVertexArray(webGLObject);
+
+    for (let attrib of vao) {
+      const vbo = attrib.vbo;
+      const vboData = this._VBOs.get(vbo);
+      this._gl.bindBuffer(this._gl.ARRAY_BUFFER, vboData.webGLObject);
+      this._gl.vertexAttribPointer(attrib.location, vbo.components, vbo.type,
+        attrib.normalized, vbo.stride, attrib.offset
+      );
+    }
+    this._gl.bindBuffer(this._gl.ARRAY_BUFFER, null);
+  }
+
+  draw(program, vao) {
+    const programData = this._programs.get(program);
+    if (!programData.webGLObject && !programData.failed) this.compile(program);
+    if (programData.failed) return;
+
+    const vaoData = this._VAOs.get(vao);
+    if (vaoData.failed) return;
+
+    if (!vaoData.webGLObject) this._updateVAO(vao);
+
+    this._gl.viewport(0, 0, this._gl.canvas.width, this._gl.canvas.height);
+
+    this._gl.clearColor(0, 0, 0, 0);
+    this._gl.clear(this._gl.COLOR_BUFFER_BIT);
+
+    this._gl.useProgram(programData.webGLObject);
+    this._gl.bindVertexArray(vaoData.webGLObject);
+
+    this._gl.drawArrays(this._gl.TRIANGLES, 0, vao.count);
   }
 
   get gl() {
